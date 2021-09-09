2633d3cc1df49239e66bb6dfffed264574dab2cf
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-09-09 20:37:08 +0800

    Initial commit

---
+++ app/Models/Author.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Author extends Model
+{
+
+    protected $fillable = [
+        'name',
+        'email',
+        'github',
+        'twitter',
+        'location',
+        'latest_article_published'
+    ];
+}
